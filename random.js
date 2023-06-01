f039f947ef56202f97551189639a7c891e80dcfb
KEBOAIBEK <102234390+KEBOAIBEK@users.noreply.github.com>
2023-06-02 00:27:46 +0500

    some changes

---
+++ random.js
@@ -0,0 +1,6 @@
+const   firstName = prompt("Enter first name"),
+        secondName = prompt("Enter second name");
+
+const randomNumber = (Math.floor(Math.random() * 100))  
+
+alert(`First subscriber ${firstName} and second subcriber ${secondName} are ${randomNumber} same with height`)
