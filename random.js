d11d15d6835849957a43c7834cdd58de5d136f49
KEBOAIBEK <102234390+KEBOAIBEK@users.noreply.github.com>
2023-06-03 15:07:31 +0500

    with if

--- random.js
+++ random.js
@@ -1,6 +1,19 @@
 const   firstName = prompt("Enter first name"),
         secondName = prompt("Enter second name");
 
-const randomNumber = (Math.floor(Math.random() * 100))  
+const randomNumber = (Math.floor(Math.random() * 1000))  
 
-alert(`First subscriber ${firstName} and second subcriber ${secondName} are ${randomNumber} same with height`)
+if (randomNumber < 150) {
+    alert(`First subscriber ${firstName} and 
+    second subcriber ${secondName} are ${randomNumber} same with height you need to go gym`)
+} else if (randomNumber <= 170) {
+    alert(`First subscriber ${firstName} and 
+    second subcriber ${secondName} are ${randomNumber} same with height`)
+} else if(randomNumber < 190){
+    alert(`First subscriber ${firstName} and 
+    second subcriber ${secondName} are ${randomNumber} same with height `)
+} else {
+    "upps something going wrong"
+}
+
+
